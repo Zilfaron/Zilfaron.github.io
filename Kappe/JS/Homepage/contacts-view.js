b2a81ca760b782e09a1a4b1eccd6d86300043310
Zilfaron <42319249+Zilfaron@users.noreply.github.com>
2018-09-09 22:14:12 +0500

    Clear

--- Kappe/JS/Homepage/contacts-view.js
+++
@@ -1,6 +0,0 @@
-var contacts_button = $(".contact-icon"),
-    info_container  = $(".info");
-
-contacts_button.bind("click", function() {
-  info_container.toggleClass("visible_info");
-});
--- Kappe/JS/Homepage/mobile-menu-view.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  function switch_view() {
-    $(".mobile-menu").slideToggle(250, "linear").scrollTop(0);
-  }
-  $(".mobile-open-btn").on("click", function() {
-    switch_view();
-  });
-});
