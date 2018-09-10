b272868934227991859986674b49ca80f9da7baf
Zilfaron <42319249+Zilfaron@users.noreply.github.com>
2018-09-10 16:19:03 +0500

    add

---
+++ Game/JS/tutorial.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function() {
+	var tutorial_lastG = document.querySelector(".tutorial__info .info__last-time"),
+			tutorial_lastW = document.querySelector(".tutorial__info .info__last-scores"),
+			start_btn			 = document.querySelector(".tutorial__start .start__btn"),
+			tutorial 			 = document.querySelector(".game .game__tutorial");
+
+	tutorial_lastG.textContent = localStorage.getItem("last-game") || "-- / -- / --";
+	tutorial_lastW.textContent = localStorage.getItem("last-scores") || "0/30";
+
+	start_btn.addEventListener("click", function() {
+		tutorial.hidden = true;
+		game.start();
+	});
+});
